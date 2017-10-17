32c0a2c5b0c1bc4ad9d817d7055892787f45a3de
Johan Bloemberg <github@ijohan.nl>
2017-10-17 20:55:59 +0200

    wip

--- failmap_admin/map/management/commands/rebuild-ratings.py
+++ failmap_admin/map/management/commands/rebuild-ratings.py
@@ -5,10 +5,30 @@ from failmap_admin.map.determineratings import (default_ratings, rate_organizati
 from failmap_admin.map.models import OrganizationRating, UrlRating
 
 
+class TaskCommand(Basecommand):
+    """A command that performs it's intended behaviour through a Celery task.
+
+    The task can be run directly, sync- and asynchronously.
+
+    Direct execution will run the task as if it was a direct function call.
+
+    Sync execution will use the Celery framework to execute the task on
+    a (remote) worker destined to execute tasks of this type. It will wait for
+    execution to complete and return the task result/logging.
+
+    Async is like Synchronous execution but it will not wait for it to complete.
+    No result or logging will be returned.
+
+    Direct and sync methods allow the task to be interupted during execution
+    using ctrl-c.
+    """
+
+
 # Remove ALL organization and URL ratings and rebuild them
-class Command(BaseCommand):
-    help = "Remove all organization and url ratings, then rebuild them from scratch. " \
-           "This can take a while."
+class Command(TaskCommand):
+    """Remove all organization and url ratings, then rebuild them from scratch."""
+
+    help = __doc__
 
     # history
     # this has been made when a bug caused hunderds of thousands of ratings, slowing the top fail
